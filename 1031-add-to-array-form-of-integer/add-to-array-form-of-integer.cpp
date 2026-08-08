7a793e75b4b1622adce786e3ec9ceefc45c0f3d4
Ritesh yadav <nexusritesh177@gmail.com>
2026-08-08 19:41:52 +0530

    Time: 0 ms (100.00%) | Memory: 32.9 MB (22.58%) - LeetSync

---
+++ 1031-add-to-array-form-of-integer/add-to-array-form-of-integer.cpp
@@ -0,0 +1,23 @@
+
+
+class Solution {
+public:
+    std::vector<int> addToArrayForm(std::vector<int>& num, int k) {
+        int i = num.size() - 1;
+        std::vector<int> result;
+
+       
+        while (i >= 0 || k > 0) {
+            if (i >= 0) {
+                k += num[i];
+                i--;
+            }
+            result.push_back(k % 10); 
+            k /= 10;                 
+        }
+
+
+        std::reverse(result.begin(), result.end());
+        return result;
+    }
+};
