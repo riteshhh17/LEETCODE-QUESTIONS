7a5fbc087050730c9914f94fdedd35491edfb64b
Ritesh yadav <nexusritesh177@gmail.com>
2026-08-20 04:52:50 +0530

    Time: 0 ms (100.00%) | Memory: 15.7 MB (85.57%) - LeetSync

--- 740-delete-and-earn/delete-and-earn.cpp
+++ 740-delete-and-earn/delete-and-earn.cpp
@@ -3,17 +3,17 @@ public:
     int deleteAndEarn(vector<int>& nums) {
     int a= *max_element(nums.begin(),nums.end());
     vector<int>vec(a+1);
-    vector<int>dp(a+1,0);
+    vector<int>ans(a+1,0);
     for(int val:nums){
         vec[val]+=val;
     }  
-    dp[1]=vec[1];
+    ans[1]=vec[1];
     for(int i=2;i<=a;i++){
-        dp[i]=max(dp[i-2]+vec[i], dp[i-1]);
+        ans[i]=max(ans[i-2]+vec[i], ans[i-1]);
 
 
     }
         
-       return dp[a]; 
+       return ans[a]; 
     }
 };
