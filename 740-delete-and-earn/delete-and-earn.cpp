59b324082ec75bd23cf5508c20e69fec9d6f680f
Ritesh yadav <nexusritesh177@gmail.com>
2026-08-20 04:49:18 +0530

    Time: 0 ms (100.00%) | Memory: 15.8 MB (66.35%) - LeetSync

--- 740-delete-and-earn/delete-and-earn.cpp
+++ 740-delete-and-earn/delete-and-earn.cpp
@@ -1,26 +1,19 @@
 class Solution {
 public:
     int deleteAndEarn(vector<int>& nums) {
-        int mx = *max_element(nums.begin(), nums.end());
+    int a= *max_element(nums.begin(),nums.end());
+    vector<int>vec(a+1);
+    vector<int>dp(a+1,0);
+    for(int val:nums){
+        vec[val]+=val;
+    }  
+    dp[1]=vec[1];
+    for(int i=2;i<=a;i++){
+        dp[i]=max(dp[i-2]+vec[i], dp[i-1]);
 
-        vector<int> points(mx + 1, 0);
 
-        // Calculate total points for each number
-        for(int x : nums) {
-            points[x] += x;
-        }
-
-        // House Robber DP
-        vector<int> dp(mx + 1, 0);
-
-        dp[0] = 0;
-        if(mx >= 1)
-            dp[1] = points[1];
-
-        for(int i = 2; i <= mx; i++) {
-            dp[i] = max(dp[i-1], points[i] + dp[i-2]);
-        }
-
-        return dp[mx];
+    }
+        
+       return dp[a]; 
     }
 };
